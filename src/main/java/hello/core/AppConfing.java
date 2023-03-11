65391acf7281764082e854c5c00f50d586205e8e
lsj <lsj930224@naver.com>
2023-03-12 02:08:28 +0900

    DIP/DI/AppConfing 리팩토링

---
+++ src/main/java/hello/core/AppConfing.java
@@ -0,0 +1,30 @@
+package hello.core;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import hello.core.member.MemoryMemberRepository;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
+
+public class AppConfing {
+    //AppConfing는 애플리케이션의 실제 동작에 필요한 구현 객체를 생성한다.
+    //생성한 객체 인스턴스의 참조(래퍼런스)를 생성자를 통해서 주입해준다.
+
+    public MemberService memberService(){
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private static MemoryMemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    public OrderService orderService(){
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy(){
+        return new FixDiscountPolicy();
+    }
+}
