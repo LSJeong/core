7ed40860a43dafb0202a8820a20b6cba78fcce6a
lsj <lsj930224@naver.com>
2023-03-12 02:43:54 +0900

    AppConfig 스프링으로 전환

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -4,13 +4,18 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     public static void main(String[] args) {
         //MemberService memberService = new MemberServiceImpl();
 
-        AppConfing appConfing = new AppConfing();
-        MemberService memberService = appConfing.memberService();
+//        AppConfing appConfing = new AppConfing();
+//        MemberService memberService = appConfing.memberService();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfing.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);//이릅,타입
 
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
--- src/main/java/hello/core/OrderApp.java
+++ src/main/java/hello/core/OrderApp.java
@@ -7,21 +7,27 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.order.Order;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
     public static void main(String[] args) {
 //        MemberService memberService = new MemberServiceImpl();
 //        OrderService orderService = new OrderServiceImpl();
 
-        AppConfing appConfing = new AppConfing();
-        MemberService memberService = appConfing.memberService();
-        OrderService orderService = appConfing.orderService();
+//        AppConfing appConfing = new AppConfing();
+//        MemberService memberService = appConfing.memberService();
+//        OrderService orderService = appConfing.orderService();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfing.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
 
         Long memberId = 1L;
         Member member = new Member(memberId,"memberA", Grade.VIP);
         memberService.join(member);
 
-        Order order = orderService.createOrder(memberId,"itemA",10000);
+        Order order = orderService.createOrder(memberId,"itemA",20000);
 
         System.out.println("order = " + order.toString());
         System.out.println("order.calculatePrice = " + order.calculatePrice());
