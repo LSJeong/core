734cf4fb16f1e10dd62caba79782bc62b39c93f1
lsj <lsj930224@naver.com>
2023-03-17 23:29:12 +0900

    Annotation 직접만들기

--- src/main/java/hello/core/discount/RateDiscountPolicy.java
+++ src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -1,10 +1,14 @@
 package hello.core.discount;
 
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.member.Grade;
 import hello.core.member.Member;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
+//@Primary  //fixDiscountPolicy도 @Component했을때 타입이 같으므로 에러가난다 => @Primary 붙은게 우선순위로 주입되어 오류나지않음
+@MainDiscountPolicy
 public class RateDiscountPolicy implements DiscountPolicy{
 
     private int discountPercent = 10;
